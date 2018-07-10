48c8a290f7e65b0ca9618a703f2c8b452ff5591d
lucas.aquino <lucas.aquino@tse.jus.br>
2018-07-10 17:10:58 -0300

    portlet fixed

--- src/pf/manuais/siproquim2/theme/js/main.js
+++ src/pf/manuais/siproquim2/theme/js/main.js
@@ -1,25 +1,67 @@
 $(document).ready(function () {
   $('.collapsible').collapsible();
+
+
+ /* HOME PAGE   */
+  function fixDivIndex() {
+    var $cache = $('.menu_flutua');
+    if ($(window).scrollTop() > 200)
+      $cache.css({
+        'position': 'fixed',
+    'top': '10%'
+
+      });
+    else
+      $cache.css({
+        'position': 'fixed',
+        'top': '40%'
+      });
+  }
+  $(window).scroll(fixDivIndex);
+  fixDivIndex();
+
+
+  /* PÁGINAS INTERNAS  */
+    function fixDiv() {
+    var $cache = $('.box_portlet_pagina');
+    if ($(window).scrollTop() > 200)
+      $cache.css({
+        'position': 'fixed',
+        'top': '10%'
+
+      });
+    else
+      $cache.css({
+        'position': 'fixed',
+        'top': '15%'
+      });
+  }
+  $(window).scroll(fixDiv);
+  fixDiv();
+
+
+
+
 });
 
 
 function sim() {
-  
+
   $('#feedback-nao').prop('disabled', true);
   $('#feedback-sim').text('Sim. Obrigado!').css('color', '#444444');
   $('#feedback-sim').css({'background-color': '#EAF1DF', 'border': '3px solid #444444'});
-  $('#feedback-text-agradecimento').show();  
-  
+  $('#feedback-text-agradecimento').show();
+
 }
 
 function nao() {
-  
-      $('#feedback-sim').prop('disabled', true); 
+
+      $('#feedback-sim').prop('disabled', true);
       $('#feedback-nao').css({
         'background-color': '#EAF1DF',
-        'color': '#444444', 'border': '3px solid #444444'}); 
+        'color': '#444444', 'border': '3px solid #444444'});
       $('#feedback-text').show();
-	  
-     
+
+
   }
 
